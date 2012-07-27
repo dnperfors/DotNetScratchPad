b75c7a4ae20903d80053054d99a580576fb6bdf1
David Perfors <dnperfors@gmail.com>
2012-07-27 14:07:05 +0200

    Initial commit for my own Linq to Object implementation

---
+++ Linq/LearningLinq/Empty.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearningLinq
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TResult> Empty<TResult>()
+        {
+            return EmptyEnumerable<TResult>.Instance;
+        }
+
+        class EmptyEnumerable<TResult>
+        {
+            public static TResult[] Instance = new TResult[0];
+        }
+    }
+}
---
+++ Linq/LearningLinqTests/EmptyTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using LearningLinq;
+using Xunit;
+
+namespace LearningLinqTests
+{
+    public class EmptyTests
+    {
+        [Fact]
+        public void Should_return_empty_enumerable()
+        {
+            IEnumerable<int> emptyEnumerable = Enumerable.Empty<int>();
+
+            Assert.Empty(emptyEnumerable);
+        }
+
+        [Fact]
+        public void Should_always_return_the_same_instance()
+        {
+            Assert.Same(Enumerable.Empty<int>(), Enumerable.Empty<int>());
+            Assert.Same(Enumerable.Empty<long>(), Enumerable.Empty<long>());
+            Assert.Same(Enumerable.Empty<double>(), Enumerable.Empty<double>());
+            Assert.Same(Enumerable.Empty<float>(), Enumerable.Empty<float>());
+            Assert.Same(Enumerable.Empty<char>(), Enumerable.Empty<char>());
+            Assert.NotSame(Enumerable.Empty<int>(), Enumerable.Empty<long>());
+        }
+    }
+}
