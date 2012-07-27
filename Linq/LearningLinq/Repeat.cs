1ba29e0f3e7ccfe1b03827262d43e1061dac8e92
David Perfors <dnperfors@gmail.com>
2012-07-27 18:09:39 +0200

    Add implementation Enumerable.Repeat

---
+++ Linq/LearningLinq/Repeat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LearningLinq
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TResult> Repeat<TResult>(TResult element, int count)
+        {
+            if (count < 0)
+                throw new ArgumentOutOfRangeException("count");
+            return RepeatImpl(element, count);
+        }
+
+        private static IEnumerable<TResult> RepeatImpl<TResult>(TResult element, int count)
+        {
+            for (int i = 0; i < count; i++)
+                yield return element;
+        }
+    }
+}
---
+++ Linq/LearningLinqTests/RepeatTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using LearningLinq;
+using Xunit.Extensions;
+
+namespace LearningLinqTests
+{
+    public class RepeatTests
+    {
+        [Fact]
+        public void Should_throw_ArgumentOutOfRangeException_when_count_is_negative()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Enumerable.Repeat(4, -1));
+        }
+
+        [Theory]
+        [InlineData(new int[] { 5, 5, 5, 5, 5 }, 5, 5)]
+        [InlineData(new object[] { null, null }, null, 2)]
+        [InlineData(new char[] {}, 'x', 0)]
+        public void Should_correctly_return_the_same_value_multiple_times<T>(T[] expected, T element, int count)
+        {
+            Assert.Equal(expected, Enumerable.Repeat(element, count));
+        }
+    }
+}
