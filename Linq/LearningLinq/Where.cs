758f99a3a8f13002ac4f2b5db49eaee6fffed2c7
David Perfors <dnperfors@gmail.com>
2012-07-31 13:19:25 +0200

    Add implementation Enumerable.Where

---
+++ Linq/LearningLinq/Where.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LearningLinq
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TSource> Where<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            if (predicate == null)
+                throw new ArgumentNullException("predicate");
+            return WhereImpl(source, predicate);
+        }
+
+        private static IEnumerable<TSource> WhereImpl<TSource>(IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            foreach (TSource item in source)
+            {
+                if (predicate(item))
+                    yield return item;
+            }
+        }
+
+        public static IEnumerable<TSource> Where<TSource>(this IEnumerable<TSource> source, Func<TSource, int, bool> predicate)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            if (predicate == null)
+                throw new ArgumentNullException("predicate");
+            return WhereImpl(source, predicate);
+        }
+
+        private static IEnumerable<TSource> WhereImpl<TSource>(IEnumerable<TSource> source, Func<TSource, int, bool> predicate)
+        {
+            int count = 0;
+            foreach (TSource item in source)
+            {
+                if (predicate(item, count))
+                    yield return item;
+                count++;
+            }
+        }
+    }
+}
---
+++ Linq/LearningLinqTests/WhereTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using LearningLinq;
+using Xunit;
+
+namespace LearningLinqTests
+{
+    public class WhereTests
+    {
+        [Fact]
+        public void Should_throw_ArgumentNullException_when_source_is_null()
+        {
+            IEnumerable<int> source = null;
+            Assert.Throws<ArgumentNullException>(() => source.Where(x => x < 4));
+            Assert.Throws<ArgumentNullException>(() => source.Where((x, index) => x < 4));
+        }
+
+        [Fact]
+        public void Should_throw_ArgumentNullException_when_predicate_is_null()
+        {
+            IEnumerable<int> source = new int[] { 2, 5, 3, 7, 1, 4 };
+            Assert.Throws<ArgumentNullException>(() => source.Where((Func<int, bool>)null));
+            Assert.Throws<ArgumentNullException>(() => source.Where((Func<int, int, bool>)null));
+        }
+
+        [Fact]
+        public void Should_return_correct_result()
+        {
+            IEnumerable<int> source = new int[] { 2, 5, 3, 7, 1, 4 };
+            Assert.Equal(new int[] { 2, 3, 1 }, source.Where(x => x < 4));
+            Assert.Equal(new int[] { 2, 3, 1 }, source.Where((x, index) => x < 4));
+        }
+
+        [Fact]
+        public void Should_give_index_to_predicate()
+        {
+            IEnumerable<int> source = new int[] { 2, 5, 3, 7, 1, 4 };
+
+            Assert.Equal(new int[] { 1, 4 }, source.Where((x, index) => x < index));
+        }
+    }
+}
