426e006dcb5093ba0f7b06d002ba735bee8d67e6
David Perfors <dnperfors@gmail.com>
2012-07-31 19:36:49 +0200

    Add implementation Enumerable.Select

---
+++ Linq/LearningLinq/Select.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearningLinq
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TResult> Select<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (selector == null) throw new ArgumentNullException("selector");
+            return SelectImpl(source, selector);
+        }
+
+        private static IEnumerable<TResult> SelectImpl<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> selector)
+        {
+            foreach (TSource item in source)
+            {
+                yield return selector(item);
+            }
+        }
+
+        public static IEnumerable<TResult> Select<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, int, TResult> selector)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (selector == null) throw new ArgumentNullException("selector");
+            return SelectImpl(source, selector);
+        }
+
+        private static IEnumerable<TResult> SelectImpl<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, int, TResult> selector)
+        {
+            int counter = 0;
+            foreach (TSource item in source)
+            {
+                yield return selector(item, counter);
+                counter++;
+            }
+        }
+    }
+}
---
+++ Linq/LearningLinqTests/SelectTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using LearningLinq;
+using Xunit;
+
+namespace LearningLinqTests
+{
+    public class SelectTests
+    {
+        [Fact]
+        public void Should_throw_ArgumentNullException_when_source_is_null()
+        {
+            IEnumerable<int> source = null;
+            Assert.Throws<ArgumentNullException>(() => source.Select(x => x * x));
+            Assert.Throws<ArgumentNullException>(() => source.Select((x, y) => x * y));
+        }
+
+        [Fact]
+        public void Should_throw_ArgumentNullException_when_selector_is_null()
+        {
+            IEnumerable<int> source = Enumerable.Range(1, 4);
+            Assert.Throws<ArgumentNullException>(() => source.Select((Func<int, bool>)null));
+            Assert.Throws<ArgumentNullException>(() => source.Select((Func<int, int, bool>)null));
+        }
+
+        [Fact]
+        public void Should_return_correct_values()
+        {
+            IEnumerable<int> source = Enumerable.Range(1, 4);
+            Assert.Equal(new int[] { 1, 4, 9, 16 }, source.Select(x => x * x));
+            Assert.Equal(new int[] { 0, 2, 6, 12 }, source.Select((x, index) => x * index));
+        }
+
+        [Fact]
+        public void Should_be_able_to_convert_valuetypes()
+        {
+            IEnumerable<int> source = Enumerable.Range(1, 4);
+            Assert.Equal(new string[] { "1", "2", "3", "4" }, source.Select(x => x.ToString()));
+            Assert.Equal(new string[] { "1", "2", "3", "4" }, source.Select((x, index) => x.ToString()));
+        }
+    }
+}
