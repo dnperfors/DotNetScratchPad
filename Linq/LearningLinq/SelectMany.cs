1ae5b5befd041ca5fa3bbfb2bc8828ca21d0f1c7
David Perfors <dnperfors@gmail.com>
2012-08-01 20:01:45 +0200

    Add implementation for Enumerable.SelectMany

---
+++ Linq/LearningLinq/SelectMany.cs
@@ -0,0 +1,94 @@
+﻿///   Copyright 2012 David Perfors
+///
+///   Licensed under the Apache License, Version 2.0 (the "License");
+///   you may not use this file except in compliance with the License.
+///   You may obtain a copy of the License at
+///
+///       http://www.apache.org/licenses/LICENSE-2.0
+///
+///   Unless required by applicable law or agreed to in writing, software
+///   distributed under the License is distributed on an "AS IS" BASIS,
+///   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+///   See the License for the specific language governing permissions and
+///   limitations under the License.
+
+using System;
+using System.Collections.Generic;
+
+namespace LearningLinq
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TResult> SelectMany<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, IEnumerable<TResult>> selector)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (selector == null) throw new ArgumentNullException("selector");
+            return SelectManyImpl(source, selector);
+        }
+
+        private static IEnumerable<TResult> SelectManyImpl<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, IEnumerable<TResult>> selector)
+        {
+            foreach (TSource item in source)
+            {
+                foreach (TResult result in selector(item))
+                    yield return result;
+            }
+        }
+
+        public static IEnumerable<TResult> SelectMany<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, int, IEnumerable<TResult>> selector)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (selector == null) throw new ArgumentNullException("selector");
+            return SelectManyImpl(source, selector);
+        }
+
+        private static IEnumerable<TResult> SelectManyImpl<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, int, IEnumerable<TResult>> selector)
+        {
+            int sourceCount = 0;
+            foreach (TSource item in source)
+            {
+                foreach (TResult result in selector(item, sourceCount))
+                {
+                    yield return result;
+                }
+                sourceCount++;
+            }
+        }
+
+        public static IEnumerable<TResult> SelectMany<TSource, TCollection, TResult>(this IEnumerable<TSource> source, Func<TSource, IEnumerable<TCollection>> collectionSelector, Func<TSource, TCollection, TResult> resultSelector)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (collectionSelector == null) throw new ArgumentNullException("collectionSelector");
+            if (resultSelector == null) throw new ArgumentNullException("resultSelector");
+            return SelectManyImpl(source, collectionSelector, resultSelector);
+        }
+
+        private static IEnumerable<TResult> SelectManyImpl<TSource, TCollection, TResult>(IEnumerable<TSource> source, Func<TSource, IEnumerable<TCollection>> collectionSelector, Func<TSource, TCollection, TResult> resultSelector)
+        {
+            foreach (TSource item in source)
+            {
+                foreach (TCollection result in collectionSelector(item))
+                    yield return resultSelector(item, result);
+            }
+        }
+
+        public static IEnumerable<TResult> SelectMany<TSource, TCollection, TResult>(this IEnumerable<TSource> source, Func<TSource, int, IEnumerable<TCollection>> collectionSelector, Func<TSource, TCollection, TResult> resultSelector)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (collectionSelector == null) throw new ArgumentNullException("collectionSelector");
+            if (resultSelector == null) throw new ArgumentNullException("resultSelector");
+            return SelectManyImpl(source, collectionSelector, resultSelector);
+        }
+
+        private static IEnumerable<TResult> SelectManyImpl<TSource, TCollection, TResult>(IEnumerable<TSource> source, Func<TSource, int, IEnumerable<TCollection>> collectionSelector, Func<TSource, TCollection, TResult> resultSelector)
+        {
+            int counter = 0;
+            foreach (TSource item in source)
+            {
+                foreach (TCollection result in collectionSelector(item, counter))
+                    yield return resultSelector(item, result);
+                counter++;
+            }
+        }
+    }
+}
---
+++ Linq/LearningLinqTests/SelectManyTests.cs
@@ -0,0 +1,90 @@
+﻿///   Copyright 2012 David Perfors
+///
+///   Licensed under the Apache License, Version 2.0 (the "License");
+///   you may not use this file except in compliance with the License.
+///   You may obtain a copy of the License at
+///
+///       http://www.apache.org/licenses/LICENSE-2.0
+///
+///   Unless required by applicable law or agreed to in writing, software
+///   distributed under the License is distributed on an "AS IS" BASIS,
+///   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+///   See the License for the specific language governing permissions and
+///   limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using LearningLinq;
+using Xunit;
+
+namespace LearningLinqTests
+{
+    public class SelectManyTests
+    {
+        [Fact]
+        public void Should_throw_ArgumentNullException_when_source_is_null()
+        {
+            IEnumerable<int> source = null;
+            Assert.Throws<ArgumentNullException>(() => source.SelectMany(x => x.ToString().ToCharArray()));
+            Assert.Throws<ArgumentNullException>(() => source.SelectMany((x, index) => (x + index).ToString().ToCharArray()));
+            Assert.Throws<ArgumentNullException>(() => source.SelectMany(x => x.ToString().ToCharArray(), (x, c) => x + ": " + c));
+            Assert.Throws<ArgumentNullException>(() => source.SelectMany((x, index) => (x + index).ToString().ToCharArray(), (x, c) => x + ": " + c));
+        }
+
+        [Fact]
+        public void Should_throw_ArguementNullException_when_selector_is_null()
+        {
+            IEnumerable<int> source = new int[] { 3, 5, 20, 15 };
+
+            Assert.Throws<ArgumentNullException>(() => source.SelectMany((Func<int, IEnumerable<char>>)null));
+            Assert.Throws<ArgumentNullException>(() => source.SelectMany((Func<int, int, IEnumerable<char>>)null));
+            Assert.Throws<ArgumentNullException>(() => source.SelectMany((Func<int, IEnumerable<char>>)null, (x, c) => x + ": " + c));
+            Assert.Throws<ArgumentNullException>(() => source.SelectMany((Func<int, int, IEnumerable<char>>)null, (x, c) => x + ": " + c));
+        }
+
+        [Fact]
+        public void Should_throw_ArgumentNullException_when_resultSelector_is_null()
+        {
+            IEnumerable<int> source = new int[] { 3, 5, 20, 15 };
+
+            Assert.Throws<ArgumentNullException>(() => source.SelectMany(x => x.ToString().ToCharArray(), (Func<int, char, string>)null));
+            Assert.Throws<ArgumentNullException>(() => source.SelectMany((x, index) => (x + index).ToString().ToCharArray(), (Func<int, char, string>)null));
+        }
+
+        [Fact]
+        public void Should_do_a_simple_select_many()
+        {
+            IEnumerable<int> source = new int[] { 3, 5, 20, 15 };
+            IEnumerable<char> expected = new char[] { '3', '5', '2', '0', '1', '5' };
+
+            Assert.Equal(expected, source.SelectMany(x => x.ToString().ToCharArray()));
+        }
+
+        [Fact]
+        public void Should_do_a_simple_SelectMany_with_index()
+        {
+            IEnumerable<int> source = new int[] { 3, 5, 20, 15 };
+            IEnumerable<char> expected = new char[] { '3', '6', '2', '2', '1', '8' };
+
+            Assert.Equal(expected, source.SelectMany((x, index) => (x + index).ToString().ToCharArray()));
+        }
+
+        [Fact]
+        public void Should_flatten_with_projection()
+        {
+            IEnumerable<int> source = new int[] { 3, 5, 20, 15 };
+            IEnumerable<string> expected = new string[] { "3: 3", "5: 5", "20: 2", "20: 0", "15: 1", "15: 5" };
+
+            Assert.Equal(expected, source.SelectMany(x => x.ToString().ToCharArray(), (x, c) => x + ": " + c));
+        }
+
+        [Fact]
+        public void Should_flatten_with_projection_and_index()
+        {
+            IEnumerable<int> source = new int[] { 3, 5, 20, 15 };
+            IEnumerable<string> expected = new string[] { "3: 3", "5: 6", "20: 2", "20: 2", "15: 1", "15: 8" };
+
+            Assert.Equal(expected, source.SelectMany((x, index) => (x + index).ToString().ToCharArray(), (x, c) => x + ": " + c));
+        }
+    }
+}
