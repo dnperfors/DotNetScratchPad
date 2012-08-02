161a7ef488d31acfd3c633915d75ccb36b94a157
David Perfors <dnperfors@gmail.com>
2012-08-02 20:02:25 +0200

    Add implementation for Enumerable.Any and Enumerable.All

---
+++ Linq/LearningLinq/All.cs
@@ -0,0 +1,33 @@
+﻿///   Copyright 2012 David Perfors
+///
+///   Licensed under the Apache License, Version 2.0 (the "License");
+///   you may not use this file except in compliance with the License.
+///   You may obtain a copy of the License at
+///
+///       http://www.apache.org/licenses/LICENSE-2.0
+///
+///   Unless required by applicable law or agreed to in writing, software
+///   distributed under the License is distributed on an "AS IS" BASIS,
+///   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+///   See the License for the specific language governing permissions and
+///   limitations under the License.
+
+using System;
+using System.Collections.Generic;
+
+namespace LearningLinq
+{
+    public static partial class Enumerable
+    {
+        public static bool All<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (predicate == null) throw new ArgumentNullException("predicate");
+
+            foreach (TSource item in source)
+                if (!predicate(item))
+                    return false;
+            return true;
+        }
+    }
+}
---
+++ Linq/LearningLinqTests/AllTests.cs
@@ -0,0 +1,69 @@
+﻿///   Copyright 2012 David Perfors
+///
+///   Licensed under the Apache License, Version 2.0 (the "License");
+///   you may not use this file except in compliance with the License.
+///   You may obtain a copy of the License at
+///
+///       http://www.apache.org/licenses/LICENSE-2.0
+///
+///   Unless required by applicable law or agreed to in writing, software
+///   distributed under the License is distributed on an "AS IS" BASIS,
+///   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+///   See the License for the specific language governing permissions and
+///   limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using LearningLinq;
+using Xunit;
+
+namespace LearningLinqTests
+{
+    public class AllTests
+    {
+        [Fact]
+        public void Should_throw_ArgumentNullException_when_source_is_null()
+        {
+            IEnumerable<int> source = null;
+            Assert.Throws<ArgumentNullException>(() => source.All(x => true));
+        }
+
+        [Fact]
+        public void Should_throw_ArgumentNullException_when_predicate_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => (new int[] { 1, 2, 3 }).All((Func<int,bool>)null));
+        }
+
+        [Fact]
+        public void Should_return_true_when_source_is_empty()
+        {
+            Assert.Equal(true, new int[0].All(x => x < 0));
+        }
+
+        [Fact]
+        public void Should_return_false_when_no_elements_match_predicate()
+        {
+            Assert.Equal(false, new int[] { 1, 2, 3 }.All(x => x < 0));
+        }
+
+        [Fact]
+        public void Should_return_false_when_some_elements_match_predicate()
+        {
+            Assert.Equal(false, new int[] { 1, 2, 3 }.All(x => (x % 2) == 0));
+        }
+
+        [Fact]
+        public void Should_return_true_when_all_elements_match_predicate()
+        {
+            Assert.Equal(true, new int[] { 2, 4, 6 }.All(x => (x % 2) == 0));
+        }
+
+        [Fact]
+        public void Should_stop_enumerating_as_soon_as_predicate_matches()
+        {
+            IEnumerable<int> source = new int[] { 10, 2, 0, 3 };
+            var query = source.Select(x => 10 / x);
+            Assert.DoesNotThrow(() => query.Any(x => x > 2));
+        }
+    }
+}
