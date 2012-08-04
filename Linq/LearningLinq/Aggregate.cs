2308b1be49b6a170e336600a7fb342e6fa7a05d8
David Perfors <dnperfors@gmail.com>
2012-08-04 23:18:32 +0200

    Add implementation for Enumerable.Aggregate

---
+++ Linq/LearningLinq/Aggregate.cs
@@ -0,0 +1,62 @@
+﻿///   Copyright 2012 David Perfors
+///
+///   Licensed under the Apache License, Version 2.0 (the "License");
+///   you may not use this file except in compliance with the License.
+///   You may obtain a copy of the License at
+///
+///       http://www.apache.org/licenses/LICENSE-2.0
+///
+///   Unless required by applicable law or agreed to in writing, software
+///   distributed under the License is distributed on an "AS IS" BASIS,
+///   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+///   See the License for the specific language governing permissions and
+///   limitations under the License.
+
+using System;
+using System.Collections.Generic;
+
+namespace LearningLinq
+{
+    public static partial class Enumerable
+    {
+        public static TSource Aggregate<TSource>(this IEnumerable<TSource> source, Func<TSource, TSource, TSource> func)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (func == null) throw new ArgumentNullException("func");
+
+            bool isEmpty = true;
+            TSource result = default(TSource);
+
+            foreach (TSource current in source)
+            {
+                result = func(result, current);
+                isEmpty = false;
+            }
+
+            if (isEmpty)
+                throw new InvalidOperationException("Empty source");
+            return result;
+        }
+
+        public static TAccumulate Aggregate<TSource, TAccumulate>(this IEnumerable<TSource> source, TAccumulate seed, Func<TAccumulate, TSource, TAccumulate> func)
+        {
+            return Aggregate(source, seed, func, x => x);
+        }
+
+        public static TResult Aggregate<TSource, TAccumulate, TResult>(this IEnumerable<TSource> source, TAccumulate seed, Func<TAccumulate, TSource, TAccumulate> func, Func<TAccumulate, TResult> resultSelector)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (func == null) throw new ArgumentNullException("func");
+            if (resultSelector == null) throw new ArgumentNullException("resultSelector");
+
+            TAccumulate result = seed;
+
+            foreach (TSource current in source)
+            {
+                result = func(result, current);
+            }
+
+            return resultSelector(result);
+        }
+    }
+}
