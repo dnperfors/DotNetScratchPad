dd50f3365f32da48f2f8bc07ebe718db3d1ef824
David Perfors <dnperfors@gmail.com>
2012-08-07 08:25:14 +0200

    Add implementation for Enumerable.Distinct

---
+++ Linq/LearningLinq/Distinct.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearningLinq
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TSource> Distinct<TSource>(this IEnumerable<TSource> source)
+        {
+            return Distinct(source, EqualityComparer<TSource>.Default);
+        }
+
+        public static IEnumerable<TSource> Distinct<TSource>(this IEnumerable<TSource> source, IEqualityComparer<TSource> comparer)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            return DistinctImpl(source, comparer);
+        }
+
+        static IEnumerable<TSource> DistinctImpl<TSource>(IEnumerable<TSource> source, IEqualityComparer<TSource> comparer)
+        {
+            HashSet<TSource> givenItems = new HashSet<TSource>(comparer);
+
+            foreach (var item in source)
+            {
+                if (givenItems.Contains(item))
+                    continue;
+
+                yield return item;
+                givenItems.Add(item);
+            }
+        }
+    }
+}
---
+++ Linq/LearningLinqTests/DistinctTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using LearningLinq;
+using Xunit;
+using System.Runtime.CompilerServices;
+
+namespace LearningLinqTests
+{
+    public class DistinctTests
+    {
+        [Fact]
+        public void Should_throw_ArgumentNullException_when_source_is_null()
+        {
+            IEnumerable<int> source = null;
+            Assert.Throws<ArgumentNullException>(() => source.Distinct());
+            Assert.Throws<ArgumentNullException>(() => source.Distinct(null));
+        }
+
+        [Fact]
+        public void Should_return_values_only_once()
+        {
+            IEnumerable<int> source = new int[] { 21, 46, 46, 55, 17, 21, 55, 55 };
+            IEnumerable<int> expected = new int[] { 21, 46, 55, 17 };
+
+            Assert.Equal(expected, source.Distinct());
+            Assert.Equal(expected, source.Distinct(null));
+        }
+
+        [Fact]
+        public void Should_return_values_only_once_according_to_own_equalityComparer()
+        {
+            IEnumerable<int> source = new int[] { 21, 46, 46, 55, 17, 21, 55, 55 };
+            IEnumerable<int> expected = new int[] { 21, 46, 46, 55, 17, 21, 55, 55 };
+
+            Assert.Equal(expected, source.Distinct(new CustomEqualityComparer()));
+        }
+
+        private class CustomEqualityComparer : IEqualityComparer<int>
+        {
+            public bool Equals(int x, int y)
+            {
+                return object.ReferenceEquals(x, y);
+            }
+
+            public int GetHashCode(int obj)
+            {
+                return RuntimeHelpers.GetHashCode(obj);
+            }
+        }
+    }
+}
