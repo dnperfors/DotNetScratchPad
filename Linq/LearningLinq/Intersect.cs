259f491decd57a7bfa58e70c0906c6570fa2fec2
David Perfors <dnperfors@gmail.com>
2012-08-11 17:01:44 +0200

    Add implementation for Enumerable.Intersect
    Alls add missing license text and an extra readme file describing where
    I got my inspiration from.

--- Linq/LearningLinq/Intersect.cs
+++ Linq/LearningLinq/Intersect.cs
@@ -19,29 +19,24 @@ namespace LearningLinq
 {
     public static partial class Enumerable
     {
-        public static IEnumerable<TSource> Union<TSource>(this IEnumerable<TSource> first, IEnumerable<TSource> second)
+        public static IEnumerable<TSource> Intersect<TSource>(this IEnumerable<TSource> first, IEnumerable<TSource> second)
         {
-            return Union(first, second, EqualityComparer<TSource>.Default);
+            return Intersect(first, second, EqualityComparer<TSource>.Default);
         }
-        public static IEnumerable<TSource> Union<TSource>(this IEnumerable<TSource> first, IEnumerable<TSource> second, IEqualityComparer<TSource> comparer)
+        public static IEnumerable<TSource> Intersect<TSource>(this IEnumerable<TSource> first, IEnumerable<TSource> second, IEqualityComparer<TSource> comparer)
         {
             if (first == null) throw new ArgumentNullException("first");
             if (second == null) throw new ArgumentNullException("second");
-            return UnionImpl(first, second, comparer ?? EqualityComparer<TSource>.Default);
+            return IntersectImpl(first, second, comparer);
         }
 
-        private static IEnumerable<TSource> UnionImpl<TSource>(IEnumerable<TSource> first, IEnumerable<TSource> second, IEqualityComparer<TSource> comparer)
+        private static IEnumerable<TSource> IntersectImpl<TSource>(IEnumerable<TSource> first, IEnumerable<TSource> second, IEqualityComparer<TSource> comparer)
         {
-            HashSet<TSource> seenElements = new HashSet<TSource>(comparer);
-            foreach (var item in first)
-            {
-                if (seenElements.Add(item))
-                    yield return item;
-            }
+            HashSet<TSource> set = new HashSet<TSource>(second, comparer);
 
-            foreach (var item in second)
+            foreach (TSource item in first)
             {
-                if(seenElements.Add(item))
+                if (set.Remove(item))
                     yield return item;
             }
         }
---
+++ Linq/LearningLinq/Readme.txt
@@ -0,0 +1,5 @@
+﻿This Enumerable implementation is largely based on EduLinq from John Skeeter.
+http://msmvps.com/blogs/jon_skeet/archive/tags/Edulinq/
+The Tests are mostly taken from John Skeeter, but most of the actual implementation
+I wrote myself. Based on the code I found in the blogposts of John, the actual .NET
+code and the Mono code, I refactored the implementation to what it is now.
