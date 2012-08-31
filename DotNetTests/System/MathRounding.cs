a07d8fc0aed3984596a3557b264931a96137c013
David Perfors <dnperfors@gmail.com>
2012-08-31 08:05:47 +0200

    Add a simple test to demonstrate how rounding works

---
+++ DotNetTests/System/MathRounding.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace DotNetTests.System
+{
+    public class MathRounding
+    {
+        [Fact]
+        public void Should_round_correctly()
+        {
+            Assert.Equal(2, Math.Round(2.5m, 0, MidpointRounding.ToEven));
+            Assert.Equal(4, Math.Round(3.5m, 0, MidpointRounding.ToEven));
+
+            Assert.Equal(2, decimal.Round(2.5m, 0));
+            Assert.Equal(3, decimal.Round(2.6m, 0));
+            Assert.Equal(3, decimal.Round(2.51m, 0));
+
+            Assert.Equal(3, Math.Round(2.5m, 0, MidpointRounding.AwayFromZero));
+            Assert.Equal(4, Math.Round(3.5m, 0, MidpointRounding.AwayFromZero));
+        }
+    }
+}
