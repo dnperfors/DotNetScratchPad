8b5f8d42b6987c19ed7bb62a2cf1f2b739c9f4a8
David Perfors <dnperfors@gmail.com>
2020-01-16 22:48:42 +0100

    Add some tests around mocking httpmessagehandlers.

---
+++ httpclient.testserver/HttpClientTests.cs
@@ -0,0 +1,40 @@
+﻿using NFluent;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace httpclient.testserver
+{
+    public class HttpClientTests
+    {
+        [Fact]
+        public async Task MockGetRequest()
+        {
+            var server = new TestServer();
+            server.RespondWith(HttpStatusCode.NotFound);
+
+            using var client = new HttpClient(server);
+
+            var result = await client.GetAsync("https://httbin.org/get");
+            
+            Check.That(result.StatusCode).IsEqualTo(HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task MockTimeout()
+        {
+            var server =new TestServer();
+            server.SimulateTimeout();
+
+            using var client = new HttpClient(server);
+
+            var result = await client.GetAsync("https://httpbin.org/get");
+
+            Check.That(result.StatusCode).IsEqualTo(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ httpclient.testserver/TestServer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace httpclient.testserver
+{
+    public class TestServer : HttpMessageHandler
+    {
+        private HttpResponseMessage response;
+        internal void RespondWith(HttpResponseMessage responseMessage)
+        {
+            response = responseMessage;
+        }
+
+        public TestServer RespondWith(HttpStatusCode statusCode)
+        {
+            RespondWith(new HttpResponseMessage
+            {
+                StatusCode = statusCode
+            });
+
+            return this;
+        }
+
+        public TestServer SimulateTimeout()
+        {
+            RespondWith(new TimeoutHttpResponseMessage());
+            return this;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var result = new TaskCompletionSource<HttpResponseMessage>();
+            if(response is TimeoutHttpResponseMessage)
+            {
+                result.SetCanceled();
+            }
+            else{
+                result.SetResult(response);
+            }
+            return result.Task;
+        }
+    }
+
+    internal class TimeoutHttpResponseMessage : HttpResponseMessage
+    {
+    }
+}
